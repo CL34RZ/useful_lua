b902044c4c98eedf581f2d539e167f6e1d449e08
Luke <yelllukesky@gmail.com>
2019-08-17 19:57:36 +0100

    Small changes, added a simple option to change the chat message

--- lua/autorun/server/dropsuit.lua
+++ lua/autorun/server/dropsuit.lua
@@ -1,10 +1,13 @@
+local msg = "!dropsuit" -- Message the player needs to type
+
 hook.Add("PlayerSay", "armorDropToFloor", function(ply, txt)
 	if !ply:IsPlayer() then return end
-	if string.lower(txt) == "!droparmor" && ply.armorSuit && ply.armorSuit ~= "" then
+	if string.lower(txt) == msg:lower() && ply.armorSuit && ply.armorSuit ~= "" then
 		local data = Armor:Get(ply.armorSuit)
 		local ent = ents.Create(data.Entitie)
 		ent:SetPos(ply:GetPos() + (ply:GetForward() * 50) + Vector(0,0,50))
 		ent:Spawn()
 		ply:removeArmorSuit()
+		return ""
 	end
 end)
